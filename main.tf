b1e7e844e7cc7401f3d835a5ad66d239fc9d51a8
Aman Shukla <amanadityashukla@gmail.com>
2023-12-21 18:46:19 +0530

    add import

--- main.tf
+++ main.tf
@@ -13,9 +13,15 @@ resource "azurerm_resource_group" "my_first_app" {
   tags = local.default_tags
 }
 
+resource "azurerm_resource_group" "rg" {
+}
+
 resource "azurerm_log_analytics_workspace" "my_first_app" {
   name                = "log-${local.stack}"
   location            = azurerm_resource_group.my_first_app.location
   resource_group_name = azurerm_resource_group.my_first_app.name
   tags = local.default_tags
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
 }
